0f8a8ca374dc0102af12969a17068e83c7a85fbe
headshigh <nischalgautam7200@gmail.com>
2025-09-27 12:11:19 +0545

    modified urls in cors config

--- src/main/java/com/leadersfault/config/CorsConfig.java
+++ src/main/java/com/leadersfault/config/CorsConfig.java
@@ -17,7 +17,15 @@ public class CorsConfig implements WebMvcConfigurer {
         "http://127.0.0.1:3000",
         "http://127.0.0.1:3001",
         "http://localhost:8080",
-        "http://127.0.0.1:8080"
+        "http://127.0.0.1:8080",
+        "https://www.nischal.tech",
+        "https://wwww.faulthub.nischal.tech",
+        "http://www.nischal.tech",
+        "http:www.faulthub.nischal.tech",
+        "https://fault-hub-frontend-1swf-74s1ajj7h-headshighs-projects.vercel.app",
+        "www.faulthub.nischal.tech",
+        "www.nischal.tech"
+
       )
       .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
       .allowedHeaders("*")
